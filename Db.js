7f04552e897fdde1dde201b3e442be4dd027e120
perception30 <perception30@gmail.com>
2019-06-23 22:09:13 +1000

    fixing error: mongo server

--- Db.js
+++ Db.js
@@ -1,55 +1,9 @@
-'use strict';
+const mongoose = require('mongoose');
 const config = require('./config');
+mongoose.connect('config.DB_SERVER',
+  { useNewUrlParser: true });
 
-const AWS = require('aws-sdk');
+const WorkLoad = mongoose.model('WorkLoad', { name: String });
 
-const params = {
-  TableName: 'SpotFaultTolerance',
-  KeySchema: [
-    { AttributeName: 'workUnit', KeyType: 'HASH' },  //Partition key
-    { AttributeName: 'title', KeyType: 'RANGE' }  //Sort key
-  ],
-  AttributeDefinitions: [
-    { AttributeName: 'workUnit', AttributeType: 'N' },
-    { AttributeName: 'title', AttributeType: 'S' }
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 10,
-    WriteCapacityUnits: 10
-  }
-};
-
-AWS.config.update({
-  region: config.AWS_REGION,
-  endpoint: config.DB_SERVER
-});
-
-class Db {
-  constructor () {
-    this.db = new AWS.DynamoDB();
-  }
-
-  createTable () {
-    this.db.createTable(params, function (err, data) {
-      if (err) {
-        console.error('Unable to create table. Error JSON:',
-          JSON.stringify(err, null, 2));
-      } else {
-        console.log('Created table. Table description JSON:',
-          JSON.stringify(data, null, 2));
-      }
-    });
-  }
-
-  getData () {
-    this.db.getItem({
-      params: {
-        TableName: ''
-      }
-    });
-  }
-
-}
-
-const db = new Db();
-db.createTable();
+const workLoad = new WorkLoad({ name: 'Scientific Application' });
+workLoad.save().then(() => console.log('worked!'));
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DB_SERVER: process.env.NODE_ENV === 'test'
     ? 'http://localhost:4569'
-    : process.env.DYNAMODB_SERVER,
+    : process.env.DB_SERVER,
   AWS_REGION: 'us-west-2'
 };
